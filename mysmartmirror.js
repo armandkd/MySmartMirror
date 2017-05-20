f12fc582f8e56a1a076f135840eb597666462bf2
Armand CADET <Armand CADET>
2017-05-20 17:34:10 +0200

    Clock style

--- mysmartmirror.js
+++ mysmartmirror.js
@@ -9,19 +9,28 @@ class Module {
 }
 
 class Time extends Module {
-	constructor(div) {
+	constructor(div, clockDiv, dateDiv) {
 		console.log("Initiating time");
 		super(div);
+		this.clockDiv = (clockDiv === undefined) ? div.find("#clock") : clockDiv;
+		this.dateDiv = (dateDiv === undefined) ? div.find("#date") : dateDiv;
 		this.updateTime();
 		setInterval(this.updateTime.bind(this), 10000);
 	}
 	updateTime() {
 		console.log("Updating time");
-		this.div.text(this.timeFormat());
+		this.date = new Date();
+		this.clockDiv.text(this.clockFormat());
+		this.dateDiv.text(this.dateFormat());
 	}
-	timeFormat() {
-		var date = new Date();
-		return date.getHours() + ":" + ("0" + date.getMinutes()).slice(-2);
+	clockFormat() {
+		
+		return this.date.getHours() + ":" + ("0" + this.date.getMinutes()).slice(-2);
+	}
+	dateFormat() {
+		var days = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+		var months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+		return days[this.date.getDay()] + " " + this.date.getDate() + " " + months[this.date.getMonth()] + " " + this.date.getFullYear();
 	}
 }
 
