ce5b95e4b217637cda08ceff27eb2d3c1dce08ce
Valourd <valentin.jean.richer@gmail.com>
2017-05-20 17:12:32 +0200

    Horaires Tan

--- mysmartmirror.js
+++ mysmartmirror.js
@@ -1,8 +1,8 @@
-class Module { 
+class Module {
 	constructor(div) {
 		this.div = div;
 	}
-	
+
 	getSize() {
 
 	}
@@ -25,9 +25,42 @@ class Time extends Module {
 	}
 }
 
+class Tan extends Module {
+	constructor(div) {
+		console.log("Initiating Tan");
+		super(div);
+		this.getTanTime();
+		setInterval(this.getTanTime.bind(this), 30000);
+	}
+
+	getTanTime() {
+		var tan = this;
+		// lien vers le JSON des temps d'attente à l'arrêt Chanzy direction Gare de Chantenay
+		var tanAPI = "http://open.tan.fr/ewp/tempsattente.json/CHZY2";
+		$.getJSON(tanAPI, function () {
+			console.log("success");
+		})
+			.done(function (TIME) {
+				console.log("second success");
+				//affichage du texte
+				tan.div.html(TIME[0].temps + "<br>" + TIME[1].temps);
+				//tan.div.text();
+			})
+			.fail(function () {
+				console.log("error");
+			})
+			.always(function () {
+				console.log("complete");
+			});
+	}
+}
+
+
+
 function init() {
 	console.log("Document loaded, initiating modules...");
 	new Time($("#time"));
+	new Tan($("#tan"));
 }
 
 $(document).ready(init);
